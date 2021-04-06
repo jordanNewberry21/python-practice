1c8364b4bd1ad5a9ac07a7c595063c7bbcbb549c
Jordan Newberry <jordan.newberry@gmail.com>
2021-04-06 14:49:54 -0500

    newfile

--- lesson20-string-methods.py
+++ lesson20-string-methods.py
@@ -38,3 +38,41 @@ print(spam.islower())
 # isspace() -- checks if string is whitespace only
 # istitle() -- titlecase only
 
+# startswith() -- checks if string starts with whatever argument is passed in
+# endswith() -- checks if string ends with whatever argument is passed in
+
+print(','.join(['cats', 'rats', 'bats']))
+print(''.join(['cats', 'rats', 'bats']))
+print(' '.join(['cats', 'rats', 'bats']))
+print('\n\n'.join(['cats', 'rats', 'bats']))
+
+print('My name is Simon'.split())
+
+# rjust() and ljust() methods will return a padded version of a string
+print('Hello'.rjust(10))
+# the first argument is an integer that is telling the method how many spaces
+# of padding to add to the string, on the right or left side as specified
+
+print('Hello'.rjust(20, '@'))
+# the second argument is a 'fill character' where instead of white space
+# it adds in whatever character is specified, this can only be one character long
+
+print('Hello'.center(20, '%'))
+# center() does the same thing as rjust and ljust but center aligns it
+
+spam = 'Hello'.center(20)
+
+print(spam)
+print(spam.strip())
+
+# strip() removes characters from a string
+# it's default behavior is to remove white spaces
+# but any characters can be passed in as an argument
+# and strip() will look through the string from the beginning and end
+# until no matching characters are found
+# there are also lstrip() and rstrip() methods to strip away from one side of the string
+
+
+spam = 'Hello there.'
+print(spam.replace('e', 'XYZ'))
+
