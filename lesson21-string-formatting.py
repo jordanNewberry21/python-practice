8c3cd60601885a9b5abef2a23b05d1bc70930b88
Jordan Newberry <jordan.newberry@gmail.com>
2021-04-06 14:57:58 -0500

    lesson 22 begins

---
+++ lesson21-string-formatting.py
@@ -0,0 +1,8 @@
+name = 'Alice'
+place = 'Main Street'
+time = '6 pm'
+food = 'turnips'
+
+print('Hello ' + name + ', you are invited to a part at ' + place + ' at ' + time + '. Please bring ' + food + '.')
+
+print('Hello %s, you are invited to a party at %s at %s. Please bring %s.' % (name, place, time, food))
