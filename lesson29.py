78adf0014bee4dee58192905638814d09bbd11a2
Jordan Newberry <jordan.newberry@gmail.com>
2021-05-03 13:21:11 -0500

    todo list

--- lesson29.py
+++ lesson29.py
@@ -1,2 +1,11 @@
-# a RegEx example program: phone and email scraper
+#! python3
 
+# TODO: Create a regex for phone numbers
+
+# TODO: Create a regex for email addresses
+
+# TODO: Get text off the clipboard
+
+# TODO: extract the email/phone numbers from this text
+
+# TODO: copy the extracted email/phone to the clipboard
