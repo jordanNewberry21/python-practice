e083e934b781499973ac6730bcf652b97c775285
Jordan Newberry <jordan.newberry@gmail.com>
2021-05-05 16:00:37 -0500

    lesson 34 done

--- lesson34.py
+++ lesson34.py
@@ -1,1 +1,13 @@
 # Walking a directory tree
+
+import os
+
+for folderName, subFolders, fileNames in os.walk('/Users/jordannewberry/Desktop/delicious'):
+    print('The folder is', folderName)
+    print('The subFolders in', folderName, 'are:', str(subFolders))
+    print('The fileNames in', folderName, 'are:', str(fileNames))
+    print()
+
+# this is the general setup for using the os.walk() method
+# here it is used in a for loop with 3 values being returned each iteration
+# these 3 values are TACOS, you can call them whatever you want
