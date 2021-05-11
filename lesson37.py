f6f013827bf904642139ffef7903bae83c9a3d5f
Jordan Newberry <jordan.newberry@gmail.com>
2021-05-11 15:05:09 -0500

    debugging test code

---
+++ lesson37.py
@@ -0,0 +1,9 @@
+# Using the Debugger
+
+print('Enter the first number to add:')
+first = int(input())
+print('Enter the second number to add:')
+second = int(input())
+print('Enter the third number to add:')
+third = int(input())
+print('The sum is ', first + second + third)
