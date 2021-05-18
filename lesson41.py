6b075fde32107a1be61e19c01b07c19071699d2d
Jordan Newberry <jordan.newberry@gmail.com>
2021-05-18 13:15:40 -0500

    selenium methods

--- lesson41.py
+++ lesson41.py
@@ -1,1 +1,22 @@
 # Controlling the Browser with the Selenium Module
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+
+browser.get('https://www.amazon.com/')
+
+# element = browser.find_element_by_css_selector('.main > div:nth-child(1) > ul:nth-child(21) > li:nth-child(1) > a:nth-child(1)')
+
+# element.click()
+
+searchElem = browser.find_element_by_css_selector('#twotabsearchtextbox')
+
+searchElem.send_keys('Marky Mark and the funky bunch')
+searchElem.submit()
+
+browser.back()
+
+browser.forward()
+browser.refresh()
+browser.quit()
