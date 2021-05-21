057ed3c8549033e96459b0fc5130e3328f5959b6
Jordan Newberry <jordan.newberry@gmail.com>
2021-05-21 14:12:15 -0500

    lesson43 donezo

--- lesson43.py
+++ lesson43.py
@@ -1,2 +1,30 @@
 # Editing Excel Spreadsheets
 
+import openpyxl
+wb = openpyxl.Workbook()
+
+sheet = wb.get_sheet_by_name('Sheet')
+
+print(sheet['A1'].value)
+
+sheet['A2'] = 42
+sheet['A1'] = 'Hello'
+import os
+
+os.chdir('/Users/jordannewberry/Desktop/')
+
+wb.save('example.xlsx')
+
+sheet2 = wb.create_sheet()
+
+wb.get_sheet_names()
+
+sheet2.title = 'My New Sheet Name'
+
+print(wb.get_sheet_names())
+
+wb.save('example2.xlsx')
+
+wb.create_sheet(index=0, title='My Other Sheet')
+
+wb.save('example3.xlsx')
