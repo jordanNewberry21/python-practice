5cec0870e9ba8f0df73b64d9d66e4ed375f38c2c
Jordan Newberry <jordan.newberry@gmail.com>
2021-05-28 16:23:50 -0500

    python stuff

--- lesson49.py
+++ lesson49.py
@@ -1,2 +1,16 @@
 # Controlling the keyboard with Python
 
+import pyautogui
+
+print(pyautogui.position())
+
+# pyautogui.click(382, 402); pyautogui.typewrite('Hello world!', interval=0.2)
+
+pyautogui.press('return')
+pyautogui.press('up')
+pyautogui.press('up')
+pyautogui.press('return')
+pyautogui.press('return')
+
+print(pyautogui.KEYBOARD_KEYS())
+
