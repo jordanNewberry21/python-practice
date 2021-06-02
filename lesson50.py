a0b74f983a795229ef6e8796662ec66370a2a715
Jordan Newberry <jordan.newberry@gmail.com>
2021-06-02 13:17:18 -0500

    got rid of some screen shots I didnt need

--- lesson50.py
+++
@@ -1,8 +0,0 @@
-# Screenshots and image recognition
-
-import pyautogui
-
-pyautogui.screenshot('/Users/jordannewberry/Desktop/screenshot_example.png')
-
-print(pyautogui.locateOnScreen('/Users/jordannewberry/Desktop/screenshot_1.png', grayscale=True, confidence=.5))
-
